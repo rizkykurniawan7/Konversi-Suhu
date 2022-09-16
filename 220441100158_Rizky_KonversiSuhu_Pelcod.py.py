2323d80c8f0d7616760dbf0532bc3b705d6d6bfd
rizkykurniawan7 <79956025+rizkykurniawan7@users.noreply.github.com>
2022-09-16 18:36:22 +0700

    Add files via upload

---
+++ 220441100158_Rizky_KonversiSuhu_Pelcod.py.py
@@ -0,0 +1,25 @@
+print("1. Celcius")
+c = 27
+print("hasil dari celcius ke fahrenhait adalah=", 9/5*c+32)
+print("hasil dari celcius ke reamur adalah=", 4/5*c)
+print("hasil dari celcius ke kelvin adalah=", c+273)
+
+print("2. Fahrenhait")
+f = 65
+print("hasil dari fahrenhait ke celcius adalah=", 5/9*(f-32))
+print("hasil dari fahrenhait ke reamur adalah=", 4/9*(f-32))
+print("hasil dari fahrenhait ke kelvin adalah=", f)
+
+print("3. Reamur")
+r = 80
+print("hasil dari reamur ke celcius adalah=", 5/4*r)
+print("hasil dari reamur ke fahrenhait adalah=", 9/4*r+32)
+print("hasil dari reamur ke kelvin adalah=", 5/4*r+273)
+
+print("4. Kelvin")
+k = 85
+print("hasil dari kelvin ke celcius adalah=", k-273)
+print("hasil dari kelvin ke fahrenhait adalah=", k)
+print("hasil dari kelvin ke reamur adalah=", 4/5*(k-273))
+
+
